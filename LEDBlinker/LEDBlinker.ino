34ccbf0ec2bb5ab36141555766f6cab39a8e1ba2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-09-07 08:27:22 +0900

    Update LEDBlinker.ino

--- LEDBlinker/LEDBlinker.ino
+++ LEDBlinker/LEDBlinker.ino
@@ -1,6 +1,18 @@
-
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000);
 }
