b22990dee067a8756329457e88ba624154a87305
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-11-11 23:10:41 +0900

    Update upload script

--- upload.sh
+++ upload.sh
@@ -13,5 +13,6 @@ echo "start compiling"
 arduino-cli compile --fqbn $fqbn $project
 echo "success compiling"
 echo "start uploading"
+sudo chmod a+rw $port
 arduino-cli upload -p $port --fqbn $fqbn $project
 echo "success uploading"
